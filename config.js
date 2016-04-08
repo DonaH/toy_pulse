14871dc351c771debe1d4cadae63117c514ffafc
dona <dona.hsieh@gmail.com>
2016-04-08 09:32:32 -0700

    prep for deploy

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secret' : 'lollan!',
-  'database': 'mongodb://localhost/toy_pulse'
-  // 'database': 'mongodb://dona:donamlab@ds015780.mlab.com:15780/toy_pulse'
+  // 'database': 'mongodb://localhost/toy_pulse'
+  'database': 'mongodb://dona:donamlab@ds015780.mlab.com:15780/toy_pulse'
 }
